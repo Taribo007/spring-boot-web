1befc5a91a2348c86ff2f107644594ecef4de150
Andrés González <Andrs Gonzlez@DESKTOP-S7BCS61>
2019-11-22 15:55:33 +0100

    Primer commit

--- spring-boot-web/src/test/java/com/bolsadeideas/springboot/web/app/SpringBootWebApplicationTests.java
+++ spring-boot-web/src/test/java/com/bolsadeideas/springboot/web/app/SpringBootWebApplicationTests.java
@@ -8,7 +8,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootWebApplicationTests {
-
+	String var;
+	
 	@Test
 	public void contextLoads() {
 		//prueba
